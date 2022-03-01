cf6f15489f92d7141215901962e2aee749c8f92a
Mahabubul Islam <mahabub2k1@gmail.com>
2022-03-01 19:52:51 +0600

    displayed phone

--- js/app.js
+++ js/app.js
@@ -4,10 +4,31 @@ const searchPhone = () => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${inputText}`;
     fetch(url)
         .then(response => response.json())
-        .then(data =>  displayPhone(data))
+        .then(data => displayPhone(data))
 }
-const displayPhone = allPhones =>{
+const displayPhone = allPhones => {
+    const phoneContainer = document.getElementById('phone-container')
     const phones = allPhones.data;
-    console.log(phones)
-
+    phones.forEach(phone => {
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+                <div class="card h-100">
+                    <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
+                    <div class="card-body">
+                        <h5 class="card-title">${phone.phone_name}</h5>
+                        <p class="card-text">${phone.brand}</p>
+                        <button class='btn btn-outline-success' onclick="showDetails('${phone.slug}')">See Specefications</button>
+                    </div>
+                </div>
+        `
+        phoneContainer.appendChild(div)
+        // console.log(phone)
+    });
+}
+const showDetails = details => {
+    const url = ` https://openapi.programming-hero.com/api/phone/${details}`
+    fetch(url)
+        .then(response => response.json())
+        .then(data => console.log(data))
 }
