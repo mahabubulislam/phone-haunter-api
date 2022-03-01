89a7ddc7833cebd2c3051761f6aa56d091fcd369
Mahabubul Islam <mahabub2k1@gmail.com>
2022-03-01 21:01:46 +0600

    details ui added

--- js/app.js
+++ js/app.js
@@ -13,7 +13,7 @@ const displayPhone = allPhones => {
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
-                <div class="card h-100">
+                <div class="card h-100 flex-lg-row flex-md-row flex-sm-column align-items-center">
                     <img src="${phone.image}" class="card-img-top w-50 mx-auto" alt="...">
                     <div class="card-body">
                         <h5 class="card-title">${phone.phone_name}</h5>
@@ -32,23 +32,31 @@ const showDetails = details => {
         .then(response => response.json())
         .then(data => displayDetails(data))
 }
-const displayDetails = info =>{
+const displayDetails = info => {
     const details = info.data;
     console.log(details)
     const detailsContainer = document.getElementById('details-container');
     const div = document.createElement('div');
-    div.innerHTML=`
-    <div class="card mb-3">
-    <img src="${details.image}" class="card-img-top" alt="...">
-    <div class="card-body">
-      <h5 class="card-title">${details.name}</h5>
-      <p class="card-text">Release Date:${details.releaseDate}</p>
-      <p class="card-text">Main features</p>
-      <p class="card-text">Storage:${details.mainFeatures.storage}</p>
-      <p class="card-text">Display Size:${details.mainFeatures.displaySiz}</p>
-      <p class="card-text">Chipset:${details.mainFeatures.chipSet}</p>
-      <p class="card-text">Memory:${details.mainFeatures.memory}</p>
-    </div>
-    `
+    div.classList.add('details')
+    div.innerHTML = `
+        <div class="card mb-3 w-100 mx-auto" style="max-width: 540px;">
+            <div class="row g-0 align-items-center">
+              <div class="col-md-4">
+                <img src="${details.image}" class="img-fluid rounded-start" alt="...">
+              </div>
+              <div class="col-md-8">
+                <div class="card-body">
+                    <h5 class="card-title">${details.name}</h5>
+                    <p class="card-text">Release Date: ${details.releaseDate}</p>
+                    <p class="card-text fw-bold fst-italic">Main features</p>
+                    <p class="card-text"><span class="fw-bold">Storage: </span> ${details.mainFeatures.storage}</p>
+                    <p class="card-text"><span class="fw-bold">Display Size:</span> ${details.mainFeatures.displaySize}</p>
+                    <p class="card-text"><span class="fw-bold">Chipset:</span> ${details.mainFeatures.chipSet}</p>
+                    <p class="card-text"><span class="fw-bold">Memory:</span> ${details.mainFeatures.memory}</p>
+                </div>
+              </div>
+            </div>
+        </div>
+                `
     detailsContainer.appendChild(div)
 }
